51f61af32a4398e42a08dc625e656654d3115e12
cristianocsl <cslcristiano@gmail.com>
2022-03-19 16:12:02 -0300

    Requisito 1 - com try/except TypeError

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
-    counter = 0
-    for register in permanence_period:
-        if not isinstance(register[0], int) or not isinstance(
-            register[1], int
-        ):
-            return None
-        elif register[0] <= target_time <= register[1]:
-            counter += 1
-    return counter
+    try:
+        counter = 0
+        for register in permanence_period:
+            if register[0] <= target_time <= register[1]:
+                counter += 1
+        return counter
+    except TypeError:
+        return
+
+
+# TypeError Exceptions may occur while performing
+# operations between two incompatible data types. Exemplo: 'str' e 'int'
+# fonte: https://www.pythonforbeginners.com/basics/
+# typeerror-in-python#:~:text=What%20is%20TypeError%20in%20Python,
+# object%20will%20not%20be%20compatible.
