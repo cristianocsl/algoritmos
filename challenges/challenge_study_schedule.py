6353a23edd902d801571b734fb20434fd6dfbce6
cristianocsl <cslcristiano@gmail.com>
2022-03-19 15:05:18 -0300

    Requisito 1 - localmente, falhou no tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,9 @@ def study_schedule(permanence_period, target_time):
         return None
     counter = 0
     for register in permanence_period:
-        if (register[0] and register[1]) is None or isinstance(register[0] or register[1], str):
+        if (register[0] and register[1]) is None or isinstance(
+            register[0] or register[1], str
+        ):
             return None
         if register[0] <= target_time <= register[1]:
             counter += 1
