c81166cfc90c62133d4b8ded485090f9f847388f
cristianocsl <cslcristiano@gmail.com>
2022-03-19 15:02:54 -0300

    Requisito 1 - localmente, falou no tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    counter = 0
+    for register in permanence_period:
+        if (register[0] and register[1]) is None or isinstance(register[0] or register[1], str):
+            return None
+        if register[0] <= target_time <= register[1]:
+            counter += 1
+    return counter
