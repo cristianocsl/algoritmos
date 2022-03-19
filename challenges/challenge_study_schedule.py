702721f4afbbe10e445a1cfccd301421c3c29dae
cristianocsl <cslcristiano@gmail.com>
2022-03-19 15:39:32 -0300

    Requisito 1 - corrige erros de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,9 @@ def study_schedule(permanence_period, target_time):
         return None
     counter = 0
     for register in permanence_period:
-        if not isinstance(register[0], int) or not isinstance(register[1], int):
+        if not isinstance(register[0], int) or not isinstance(
+            register[1], int
+        ):
             return None
         elif register[0] <= target_time <= register[1]:
             counter += 1
