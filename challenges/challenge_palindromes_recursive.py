c8dbc748990ffa6ddbe382df5502b91f914fe8a7
cristianocsl <cslcristiano@gmail.com>
2022-03-20 00:42:44 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,6 @@
+# http://computertutor.in/resources/program-8-write-a-recursive-python-program-to-test-if-a-string-is-a-palindrome-or-not/
 def is_palindrome_recursive(word, low_index, high_index):
-    if word == "" or word[low_index] != word[high_index]:
+    if word == 0 or word[low_index] != word[high_index]:
         return False
     if low_index < high_index:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
