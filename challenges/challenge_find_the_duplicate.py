9d3cc79d1d036bf99b236ffe1f922bc49ea2db94
cristianocsl <cslcristiano@gmail.com>
2022-03-23 04:29:46 -0300

    Requisito 4 - falha por excesso de complexidade

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,35 @@
+def my_sort(array):
+    for index in range(len(array)):
+        current_value = array[index]
+        current_position = index
+        while (
+            current_position > 0
+            and array[current_position - 1] > current_value
+        ):
+            array[current_position] = array[current_position - 1]
+            current_position = current_position - 1
+        array[current_position] = current_value
+    return array
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    ordered_nums = my_sort(nums)
+    for index in range(len(ordered_nums)):
+        if not isinstance(ordered_nums[index], int) or ordered_nums[index] < 0:
+            return False
+        low_index = 0
+        high_index = len(ordered_nums) - 1
+        while high_index > low_index:
+            middle_index = (high_index + low_index) // 2
+
+            if (
+                ordered_nums[index] == ordered_nums[middle_index]
+                and index != middle_index
+            ):
+                return ordered_nums[index]
+
+            elif ordered_nums[index] < ordered_nums[middle_index]:
+                high_index = middle_index - 1
+            else:
+                low_index = middle_index + 1
+    return False
