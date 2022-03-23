bb07bd5fbd3c38d8b6d5964e26969e97abb43b73
cristianocsl <cslcristiano@gmail.com>
2022-03-23 15:15:00 -0300

    Requisito 4 - corrige excesso de complexidade

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -12,24 +12,29 @@ def my_sort(array):
     return array
 
 
+def binary_search(ordered_nums, index):
+    low_index = 0
+    high_index = len(ordered_nums) - 1
+    while high_index > low_index:
+        middle_index = (high_index + low_index) // 2
+        if (
+            ordered_nums[index] == ordered_nums[middle_index]
+            and index != middle_index
+        ):
+            return ordered_nums[index]
+
+        if ordered_nums[index] < ordered_nums[middle_index]:
+            high_index = middle_index - 1
+        else:
+            low_index = middle_index + 1
+    return False
+
+
 def find_duplicate(nums):
     ordered_nums = my_sort(nums)
     for index in range(len(ordered_nums)):
         if not isinstance(ordered_nums[index], int) or ordered_nums[index] < 0:
             return False
-        low_index = 0
-        high_index = len(ordered_nums) - 1
-        while high_index > low_index:
-            middle_index = (high_index + low_index) // 2
-
-            if (
-                ordered_nums[index] == ordered_nums[middle_index]
-                and index != middle_index
-            ):
-                return ordered_nums[index]
-
-            elif ordered_nums[index] < ordered_nums[middle_index]:
-                high_index = middle_index - 1
-            else:
-                low_index = middle_index + 1
+        if binary_search(ordered_nums, index):
+            return binary_search(ordered_nums, index)
     return False
